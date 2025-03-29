cd6f2e04dd6321cefa702e3b8584d2431168c758
ouqingliang <1692110604@qq.com>
2025-03-30 00:50:17 +0800

    qaq

--- hdu.py
+++ hdu.py
@@ -8,18 +8,23 @@ import time
 
 def login(cid, username, password):
     url = f"http://acm.hdu.edu.cn/contest/login?cid=" + str(cid) + "&redirect=/contest/problems%3Fcid%3D" + str(cid)
-    r = requests.post(url, data = urllib.parse.urlencode({'username': username, 'password': password}),headers={
-        "content-type": "application/x-www-form-urlencoded",
-    }, allow_redirects=False)
-    if r.status_code != 302:
-        print("HDU contest " + str(cid) + " login failed.")
-        return None
-    return r.cookies
+
+    for i in range(3):
+        r = requests.post(url, data = urllib.parse.urlencode({'username': username, 'password': password}),headers={
+            "content-type": "application/x-www-form-urlencoded",
+        }, allow_redirects=False)
+        
+        if r.status_code == 302:
+            return r.cookies
+        time.sleep(1)
+    print("HDU contest " + str(cid) + " login failed.")
+    return None
 
 def export_csv(cid, filename, username, password):
     cookie = login(cid, username, password)
     if cookie is None:
         return
+    time.sleep(2)
     url = f"http://acm.hdu.edu.cn/contest/rank?cid={cid}&export=csv"
     r = requests.get(url, cookies = cookie)
     if r.headers.get('Content-Disposition') is None or not r.headers.get('Content-Disposition').startswith("attachment"):
@@ -39,7 +44,7 @@ def hdu_main(username, password):
     
     for i in range(len(hdu_contests_cids)):
         export_csv(hdu_contests_cids[i], "hd" + str(i + 1), username, password)
-        time.sleep(5)
+        time.sleep(3)
 
 
 if __name__ == "__main__":
